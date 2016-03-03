82fc7ccfa7f8506c422060f7d920fb20ba4c6097
sheen <ysmsmno1@gmail.com>
2016-03-03 18:43:34 +1100

    update a user check for login

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,6 +4,10 @@ class Api::V1::SessionsController < Api::V1::BaseController
     user_password=params[:session][:password]
     user_email=params[:session][:email]
     user= User.find_by(email: user_email) if user_email.present?
+    if user.nil?
+        render json: { errors: "this email doesn't register"}, status: 422
+        return
+    end
     if user.valid_password? user_password
       sign_in user, store: false
       user.generate_authentication_token!
